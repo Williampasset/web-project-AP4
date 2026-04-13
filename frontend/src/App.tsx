51c7cdda58392775443855e58f5fa0a2436809cf
ClementFlament <cflam09@gmail.com>
2026-04-13 12:04:00 +0200

    feat(routes): add Command page route to App component and update Stock page layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router'
 import Login from './pages/login/Login'
 import Affectation from './pages/Affectation/Affectation'
 import Stock from './pages/Stock/Stock'
+import Command  from './pages/Command/Command'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Login/>}></Route>
       <Route path="/affectation" element={<Affectation/>}></Route>
       <Route path="/stock" element={<Stock/>}></Route>
+      <Route path="/commande" element={<Command/>}></Route>
     </Routes>
   )
 }
--- frontend/src/pages/Stock/Stock.tsx
+++ frontend/src/pages/Stock/Stock.tsx
@@ -3,7 +3,7 @@ import DefaultLayout from '@component/default';
 export default function Stock(){
     return (
         <DefaultLayout>
-            
+            <h1>Suivis du stock</h1>
         </DefaultLayout>
     );
 }
