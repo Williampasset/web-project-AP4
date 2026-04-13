76758cdfca8f29efb54a7abfec023f8f43c74cd8
ClementFlament <cflam09@gmail.com>
2026-04-13 11:05:42 +0200

    feat(affectation): add Affectation page and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Route, Routes } from 'react-router'
 import Login from './pages/login/Login'
+import Affectation from './pages/Affectation/Affectation'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Login/>}></Route>
+      <Route path="/affectation" element={<Affectation/>}></Route>
     </Routes>
   )
 }
---
+++ frontend/src/pages/Affectation/Affectation.tsx
@@ -0,0 +1,8 @@
+import './Affectation.css';
+
+export default function Affectation(){
+    
+    return (
+        <h1>Affectation des commandes</h1>
+    );
+}
