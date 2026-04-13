1d4d4398d1b971b2c5abefeaff26905300b34c37
Williampasset <william.passet@student.junia.com>
2026-04-13 08:11:26 +0200

    feat(login): add a small form for login

--- frontend/src/login/Login.tsx
+++ frontend/src/login/Login.tsx
@@ -1,4 +1,25 @@
+import { useState, type FormEvent } from 'react';
+import './login.css';
+import { useNavigate } from 'react-router';
 
 export default function Login(){
-    return <div className="login-container"></div>
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        console.log("UserName=",userName);
+    }
+    
+    return (
+        <div className="login-container">
+            <div className='form' onSubmit={handleSubmit}>
+                <h1>Connexion</h1>
+                <input type="text" value={userName} onChange={(event) => setUserName(event.target.value)}/>
+                <input type="text" value={password} onChange={(event) => setPassword(event.target.value)}/>
+                <button type="submit" className='login-button'>Se connecter</button>
+            </div>
+        </div>
+    );
 }
