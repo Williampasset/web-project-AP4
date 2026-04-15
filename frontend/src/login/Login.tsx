eb2b9f5f9614759d85e49a658081ec7978a11aaf
Williampasset <william.passet@student.junia.com>
2026-04-15 22:46:16 +0200

    feat(login): enhance login form with async submission and success handling

--- frontend/src/login/Login.tsx
+++ frontend/src/login/Login.tsx
@@ -2,24 +2,51 @@ import { useState, type FormEvent } from 'react';
 import './login.css';
 import { useNavigate } from 'react-router';
 
-export default function Login(){
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+export default function Login() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        console.log("UserName=",userName);
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const result = await fetch('http://localhost:3000/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          matricule: userName,
+          password: password,
+        }),
+      }).then((res) => res.json());
+      console.log('Result: ', result);
+      if (result.success) {
+        console.log('Success');
+      }
+    } catch (error) {
+      console.error(error);
     }
-    
-    return (
-        <div className="login-container">
-            <div className='form' onSubmit={handleSubmit}>
-                <h1>Connexion</h1>
-                <input type="text" value={userName} onChange={(event) => setUserName(event.target.value)}/>
-                <input type="text" value={password} onChange={(event) => setPassword(event.target.value)}/>
-                <button type="submit" className='login-button'>Se connecter</button>
-            </div>
-        </div>
-    );
+  };
+
+  return (
+    <div className='login-container'>
+      <form className='form' onSubmit={handleSubmit}>
+        <h1>Connexion</h1>
+        <input
+          type='text'
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <input
+          type='text'
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type='submit' className='login-button'>
+          Se connecter
+        </button>
+      </form>
+    </div>
+  );
 }
