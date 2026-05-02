c6146e9004fbf3dc7a0a8e6ae7b7b954455257ee
Williampasset <william.passet@student.junia.com>
2026-05-02 12:08:35 +0200

    feat(articles): init articles api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, ArticlesModule],
   controllers: [AppController],
   providers: [AppService],
 })
