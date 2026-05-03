93cda9b1c84d95f2294fe8fb74daa391614ea910
Williampasset <william.passet@student.junia.com>
2026-05-03 21:55:05 +0200

    fix(app): remove app service and controller as it is useless

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ArticlesModule } from './articles/articles.module';
@@ -15,7 +13,5 @@ import { SuppliersModule } from './suppliers/suppliers.module';
     SuppliersModule,
     CommandsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
