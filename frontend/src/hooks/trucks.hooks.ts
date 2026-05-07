b1d7a3b306621bfeb494cdcdef83c0067592ecb8
Williampasset <william.passet@student.junia.com>
2026-05-07 12:59:10 +0200

    refactor(hooks): add commentary to all the hooks

--- frontend/src/hooks/trucks.hooks.ts
+++ frontend/src/hooks/trucks.hooks.ts
@@ -2,6 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchTrucks } from '../service/api/trucks.service';
 import type { Truck } from '@type/truck.type';
 
+/**
+ * Fetches trucks from the API and provides them to components.
+ * @returns An object containing the trucks data, loading state, and error state
+ */
 export const useTrucks = () => {
   return useQuery<Truck[]>({
     queryKey: ['trucks'],
